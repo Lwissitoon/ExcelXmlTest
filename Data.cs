c53c6477ac111842837fd9b555ee23e1e4ecf0a6
Luis Miguel Mateo <luismateo9594@gmail.com>
2018-01-20 22:05:28 -0400

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+using Spire.Xls;
+
+namespace DataConvertion
+
+{
+    public class Data
+    {
+
+        public static void exceltoxml()
+        {
+            Workbook wb = new Workbook();
+            wb.LoadFromFile("Nombres.xlsx");
+            wb.SaveAsXml("Nombres.xml");
+
+            wb.LoadFromFile("Saldos.xlsx");
+            wb.SaveAsXml("Saldos.xml");
+
+            wb.LoadFromFile("Cuentas.xlsx");
+            wb.SaveAsXml("Cuentas.xml");
+        }
+
+
+        public static string[] GetNombre()
+        {
+            XmlDocument reader = new XmlDocument();
+            reader.Load("Nombres.xml");
+            List<string> dato = new List<string>();
+            XmlNodeList elemList = reader.GetElementsByTagName("Data");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                dato.Add(elemList[i].InnerXml);
+            }
+
+            return dato.ToArray();
+
+        }
+
+        public static string[] GetCuenta()
+        {
+            XmlDocument reader = new XmlDocument();
+            reader.Load("Saldos.xml.xml");
+            List<string> dato = new List<string>();
+            XmlNodeList elemList = reader.GetElementsByTagName("Data");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                dato.Add(elemList[i].InnerXml);
+            }
+
+            return dato.ToArray();
+
+        }
+
+
+        public static string[] GetSaldo()
+        {
+            XmlDocument reader = new XmlDocument();
+            reader.Load("Saldos.xml");
+            List<string> dato = new List<string>();
+            XmlNodeList elemList = reader.GetElementsByTagName("Data");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                dato.Add(elemList[i].InnerXml);
+            }
+
+            return dato.ToArray();
+
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace DataConvertion
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Data.exceltoxml();
+        }
+
+        [Test]
+        public void TestMethod1(string nombre)
+        {
+
+            Assert.AreEqual(nombre,nombre);
+
+        }
+    }
+}
